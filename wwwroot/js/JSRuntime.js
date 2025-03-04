ac62677b30e3a2ef793d5e3242c6e4c5dbb85ffa
Lebed Daniil <147606047+KarakulGek@users.noreply.github.com>
2025-03-04 18:16:00 +0300

    Add files via upload

---
+++ wwwroot/js/JSRuntime.js
@@ -0,0 +1,32 @@
+﻿export function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + bytesBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+export function writeCookie(name, value, days)
+{
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+export function getCookieValue(name) {
+    const regex = new RegExp(`(^| )${name}=([^;]+)`)
+    const match = document.cookie.match(regex)
+    if (match) {
+        return match[2]
+    }
+}
+export function removeCookie(name)
+{
+    document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
